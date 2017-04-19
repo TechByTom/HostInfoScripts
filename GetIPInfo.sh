aa354f22de61f9d3580e26779160fad1a7e704a5
TechByTom <TechByTom@users.noreply.github.com>
2017-04-18 20:01:50 -0500

    Fixed newlines in dig results

--- GetIPInfo.sh
+++ GetIPInfo.sh
@@ -11,11 +11,14 @@ function IPLookup(){
 	geoIP=$(geoiplookup $1 | grep "GeoIP City Edition" | cut -d"," -f4,5)
 
 	if [ "$strHostname" ]; then
-		echo -e "Hostname  : $(tput setaf 3)$strHostname$(tput sgr 0)"
-	fi 	
-        echo -e "Look up IP: $(tput setaf 3)$1$(tput sgr 0)"
-        echo -e "Located in:$(tput setaf 3)$geoIP$(tput sgr 0)"
-        echo -e "The ISP is:$(tput setaf 3)$ISP$(tput sgr 0)"
+		echo -e "Hostname    : $(tput setaf 3)$strHostname$(tput sgr 0)"
+	fi
+        if [ "$strRevHostname" ]; then
+                echo -e "RevHostname : $(tput setaf 3)$strRevHostname$(tput sgr 0)"
+        fi 	
+        echo -e "IP Looked up: $(tput setaf 3)$1$(tput sgr 0)"
+        echo -e "Located in  :$(tput setaf 3)$geoIP$(tput sgr 0)"
+        echo -e "The ISP is  :$(tput setaf 3)$ISP$(tput sgr 0)"
 	echo
 }
 
@@ -37,14 +40,18 @@ else
 			do
 				strHostname=$1
 				if [ $(grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"<<< "$line") ]; then
+					#strRevHostname=$(dig +short -x $line | tr -d '\n' | sed 's/\.,/ /g'|sed 's/\ /, /g')
+					strRevHostname=$(dig +short -x $line | tr '\n' ' ')
 					IPLookup "$line"
+					unset strRevHostname
+                                	unset strHostname
 				fi
-				#IPLookup "$line"
-				unset strHostname
 			done
 		fi
-	else
+	else #Then it was an IP
 		publicIP=$1
+		strRevHostname=$(dig +short -x $publicIP | tr '\n' ' ')
 		IPLookup $publicIP
+		unset strRevHostname
 	fi
 fi
